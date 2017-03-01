65eafcd72ab469b71dbbdd2abb101a76cf3a72a8
DiabloH <hjy3376@gmail.com>
2017-03-01 15:56:56 +0900

    완료. 힌트 외 편의기능 업데이트

--- battleship.js
+++ battleship.js
@@ -14,14 +14,14 @@ window.onload = function(){
 	}
 
 
-		function tableDBinsert(){
-				var line = ""; 		// 라인별 정보를 종합하고, 모은 정보를 tds에 담음.
-				var size;
+	function tableDBinsert(){
+			var line = ""; 		// 라인별 정보를 종합하고, 모은 정보를 tds에 담음.
+			var size;
 
-				for(var i=0; i<tds.length; i++){
-					size = i%trs;
-					// 격자판 라인 정보를 판단하고, 자동적으로 id 및 value 지정
-					// ex) 7*7 그리드 격자판 일 때 \^[A-G]{1}[0-6]{1}$\
+			for(var i=0; i<tds.length; i++){
+				size = i%trs;
+				// 격자판 라인 정보를 판단하고, 자동적으로 id 및 value 지정
+				// ex) 7*7 그리드 격자판 일 때 \^[A-G]{1}[0-6]{1}$\
 						if( i < trs ){
 							line = mark[0]+size; // "A"
 							tds[i].value = line;
@@ -60,14 +60,15 @@ var ctrl = {
 		guessCnt : 0
 	, sunkCnt : 0
 	, hintCnt : 0
-	, hintCntUp : function(){++this.hintCnt}
-	, guessCntUp : function(){++this.guessCnt;}
-	, sunkCntUp : function(){++this.sunkCnt;}
 	, text : function(){
 		return 	this.sunkCnt/this.guessCnt>=1?"놀랍군요! 엄청난 감각 입니다.":
 						this.sunkCnt/this.guessCnt>=0.8?"상당한 실력이군요.":
 						this.sunkCnt/this.guessCnt>=0.6?"조금 할 줄 아시는군요?":
 						this.sunkCnt/this.guessCnt>=0.4?"위험했어요.":"맙소사, 살아남은게 다행입니다."
+	}
+	, text2 : function(){
+		return this.hintCnt==0?"힌트를 사용하지 않다니, 대단합니다.":
+					 this.hintCnt>=(model.shipsArray.length/3)?"힌트를 적절히 썼군요.":"큰 도움없이 잘했습니다."
 	}
 	, destroyChk : function(){
 		if(this.sunkCnt == model.shipsArray.length){
@@ -75,45 +76,19 @@ var ctrl = {
 			alert(
 				"모든 적 전함을 침몰 시켰습니다.\n"+
 				"파괴한 전함 : "+this.sunkCnt+"대\n"+
-				"명중률 : "+(this.sunkCnt/this.guessCnt)*100+"%\n"+
+				"명중률 : "+Math.floor((this.sunkCnt/this.guessCnt)*100)+"%\n"+
+				"힌트 : "+ctrl.hintCnt+"회 사용["+this.text2()+"]\n"+
 				"평판 : "+this.text()
-			)
+			);
 		}
 	} // destroyChk END
-} // ctrl End
-
-
-
-
-
-
-
-function targetCheck(event){	// table의 타겟의 value 값을 가져오고 input에 값 할당
-	document.getElementById("guessInput").value=event.target.value;
-} // targetCheck END
-
-
-function inputChk(){
-	var guessPoint = document.getElementById("guessInput").value;
-
-	for(var i=0; i<attacked.length; i++){
-		if(attacked[i] == guessPoint){
-			return alert("이미 공격했던 좌표 입니다.");
-		}
-	}
-
-	if(!regChk.test(guessPoint)){
-		alert("형식에 맞게 좌표계를 입력해주세요.");
-	}else{
-		alert("입력한 좌표로 공격합니다!");
-		ctrl.guessCntUp();
-		model.fire(guessPoint);
-	}
-} // inputChk END
+} ////////////////////////// ctrl End /////////////////////////////////
 
 
 var view = {
-			displayMessage : function(msg){
+			hints : []
+		,	hintTurn : 0
+		,	displayMessage : function(msg){
 				document.getElementById("messageArea").innerHTML=msg;
 			},
 
@@ -124,24 +99,28 @@ var view = {
 			displayMiss : function(location){
 				document.getElementById(location).setAttribute("class", "miss");
 			},
-
 			hint : function(){
-				var hints = [];
+
 				for(var i=0; i<model.shipsArray.length; i++){
 					if(regChk.test(model.shipsArray[i])){
-						hints.push(model.shipsArray[i]);
+						this.hints.push(model.shipsArray[i]);
 					}
 				}
 
 				if(ctrl.hintCnt < model.shipsArray.length/3){
-					ctrl.hintCntUp();
-					var ranHint = Math.floor( Math.random()*hints.length );
-					document.getElementById(hints[ranHint]).setAttribute("class","hint");
+					if(this.hintTurn === 0){
+						var randomHint = Math.floor(Math.random()*this.hints.length);
+						ctrl.hintCnt++;
+						this.hintTurn++;	// 한 턴에 한번만 힌트를 쓸 수 있도록 처리.
+						document.getElementById(this.hints[randomHint]).setAttribute("class","hint");
+					}else{
+						alert("기회가 남아 있다면, 다음 턴에 사용할 수 있습니다.")
+					}
 				}else{
-					alert("힌트는 이미 많이 사용 하셨는데요?");
+					alert("이미 힌트는 많이 사용하셨는데요?");
 				}
-				hints.length = 0;
 			}
+
 } // View End
 
 
@@ -152,16 +131,16 @@ var model = {
 			, isSunk : false
 			, flag : false
 			,	fire : function(guessPoint){
+
+					this.flag = false;
 					for(var i=0; i<this.shipsArray.length; i++){
 						if(this.shipsArray[i] == guessPoint){
-							this.flag = true;
-							ctrl.sunkCntUp();
-							this.shipsArray[i].value = "hit";
-							attacked.push(guessPoint);
+							alert("적중 했습니다 !");
+							this.shipsArray[i] = "hit";
 							view.displayHit(guessPoint);
 							view.displayMessage("전함 격침 !");
-							alert("적중 했습니다 !");
-
+							ctrl.sunkCnt++;
+							this.flag = true;
 						}
 					}
 
@@ -170,7 +149,9 @@ var model = {
 							view.displayMessage("실패");
 							alert("이곳이 아닌가 봅니다.");
 					}
-					flag = false;
+					attacked.push(guessPoint);
+					view.hints.length = 0; // 힌트배열에 push된 값 초기화.
+					view.hintTurn = 0; // 1회 공격 후 힌트를 사용할 수 있도록 설정.
 					ctrl.destroyChk();
 			}
 			, createShip : function(){
@@ -234,8 +215,33 @@ var model = {
 		} // Model End
 
 
-		function giveUp(){ // 포기 버튼을 눌렀을 때 모든 전함의 위치를 표시.
-			for(var i=0; i<model.shipsArray.length; i++){
-				document.getElementById(model.shipsArray[i]).setAttribute("class","hit");
-			}
+
+function targetCheck(event){	// table의 타겟의 value 값을 가져오고 input에 값 할당
+	document.getElementById("guessInput").value=event.target.value;
+} // targetCheck END
+
+
+function inputChk(){
+	var guessPoint = document.getElementById("guessInput").value;
+
+	for(var i=0; i<attacked.length; i++){
+		if(attacked[i] == guessPoint){
+			return alert("이미 공격했던 좌표 입니다.");
 		}
+	}
+
+	if(!regChk.test(guessPoint)){
+		alert("형식에 맞게 좌표계를 입력해주세요.");
+	}else{
+		alert("입력한 좌표로 공격합니다!");
+		ctrl.guessCnt++;
+		model.fire(guessPoint);
+	}
+} // inputChk END
+
+
+function giveUp(){ // 포기 버튼을 눌렀을 때 모든 전함의 위치를 표시.
+	for(var i=0; i<model.shipsArray.length; i++){
+		document.getElementById(model.shipsArray[i]).setAttribute("class","hit");
+	}
+}
